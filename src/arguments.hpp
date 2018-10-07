4b5957b81bab0f4206c1d825c0206a1ef6611005
tiberiumdvk <dan6365282@gmail.com>
2018-10-07 22:16:04 +0200

    remove object cli component

--- src/arguments.hpp
+++ src/arguments.hpp
@@ -5,7 +5,9 @@
 struct arguments
 {
     arguments(int argc, char** argv);
+    
     std::string command;
-    std::string object;
     std::map<std::string, std::string> flags;
+
+    bool has_flag(const std::string& flag);
 };
