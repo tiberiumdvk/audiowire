86c579ed51b415920925f1f234c15e0afb9dd731
tiberiumdvk <dan6365282@gmail.com>
2018-10-07 22:14:40 +0200

    create script for filling test data

---
+++ fill.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+$1 new --title Symfonia CPP --artist Nozdrzyk --album SCI
+$1 new --title Oda do asm --artist Nozdrzyk --album SCI
+$1 new --title Zakazany owoc C# --artist Nozdrzyk --album SCI
+$1 new --title Etiuda C --artist Nozdrzyk --album SCI
+$1 new --title Codzienna depresja --artist Blender --album SCI
+$1 new --title Modele z Indii --artist Blender --album SCI
+$1 new --title Kopiowanie od bubla --artist Blender --album SCI
+$1 new --title Bo Debian jest hot --artist Bonq --album SCI
+$1 new --title Shell nienawisci --artist Bonq --album SCI
+$1 new --title Jestes taka /dev/urandom --artist Bonq --album SCI
+$1 new --title Nasz Filesystem --artist Bonq --album SCI
+$1 new --title Do SCI wstapic chcialem --artist Unknown --album Delta
+$1 new --title Na IPady nie ma rady --artist Unknown --album Delta
+$1 new --title Debata czarnego --artist Unknown --album Delta
